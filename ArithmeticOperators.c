77b79fccfbfbc48df18ca6c0c761c7b7f35b9ee2
Saad-Mallebhari <saadmallebhari82@gmail.com>
2024-10-27 17:55:26 +0530

    Add files via upload

---
+++ ArithmeticOperators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 24, b = 54; // Here I have assign values to a and b
+    printf("\nArithmetic Operators:\n"); // Display the operator name
+
+    printf("a + b = %d\n", a + b);      // It will add a & b
+
+    printf("a - b = %d\n", a - b);      // It will subtract a & b
+
+    printf("a * b = %d\n", a * b);      // It will multiply a & b
+
+    printf("a / b = %d\n", a / b);      // It will divide a & b and give the quotient
+
+    printf("a %% b = %d\n", a % b);     // It will give the remainder of a & b
+
+    return 0;
+}
