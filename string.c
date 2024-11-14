cea7b19fb8deb05aff4c763a977f31be73808bca
Saad-Mallebhari <saadmallebhari82@gmail.com>
2024-11-14 14:41:02 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int arr[2][3] = {{1,2,3}, {4,5,6}};
+    for(int i =0; i<2;i++){
+        for(int j=0; j<3;j++){
+            printf("arr[%d][%d] = %d\n",i,j ,arr[i][j]);
+        }
+    }
+    return 0;
+}
